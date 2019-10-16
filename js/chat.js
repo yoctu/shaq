ceb0cba9b5c4523627c6b9c4407aa75b91bd0e74
git <git@yoctu.com>
2019-10-16 14:25:44 -0400

    test chat

--- js/chat.js
+++ js/chat.js
@@ -4,12 +4,3 @@ $zoho.salesiq = $zoho.salesiq || {
   values: {},
   ready: function() {}
 };
-var d = document;
-s = d.createElement("script");
-s.type = "text/javascript";
-s.id = "zsiqscript";
-s.defer = true;
-s.src = "https://salesiq.zoho.com/widget";
-t = d.getElementsByTagName("script")[0];
-t.parentNode.insertBefore(s, t);
-d.write("<div id='zsiqwidget'></div>");
