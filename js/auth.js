ee372cce3b25046e7d3425d89f3c0670e2c7756b
git <git@yoctu.com>
2020-01-16 16:37:58 -0500

    update version

--- js/auth.js
+++ js/auth.js
@@ -2,7 +2,7 @@ window.dataLayer = window.dataLayer || [];
 
 var auth = {};
 auth.auth = {};
-auth.webversion = "1.2.0";
+auth.webversion = "1.3.0";
 
 function getCookie(cname) {
   var name = cname + "=";
