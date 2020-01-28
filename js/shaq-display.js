84ade575938115dffbb07e562ad33217fc79aab1
Odran HUSSON <ohusson55@gmail.com>
2020-01-28 16:15:07 +0100

    Remove refs to yoctu website, replace them  by shaq-view

--- js/shaq-display.js
+++ js/shaq-display.js
@@ -500,10 +500,10 @@ function bidRefresh(bidInfo, bid) {
   }
   bidInfo.find(".bidBidderRatingScore").html("");
   for (var ratingCpt = 0; ratingCpt < bid.targetRating; ratingCpt++) {
-    bidInfo.find(".bidBidderRatingScore").append('<img width="32" src="https://yoctu.github.io/yoctu-website/img/logo/rating1.png" />');
+    bidInfo.find(".bidBidderRatingScore").append('<img width="32" src="https://yoctu.github.io/shaq-view/img/rating1.png" />');
   }
   for (ratingCpt; ratingCpt < 5; ratingCpt++) {
-    bidInfo.find(".bidBidderRatingScore").append('<img width="32" src="https://yoctu.github.io/yoctu-website/img/logo/rating0.png" />');
+    bidInfo.find(".bidBidderRatingScore").append('<img width="32" src="https://yoctu.github.io/shaq-view/img/rating0.png" />');
   }
   bidInfo.find('.bidBidderName').html('<label>' + bid.from + '</label>');
   bidInfo.find('.bidCurrency').attr('disabled', 'disabled');
@@ -721,13 +721,13 @@ function shaqRefresh() {
     $("#bid-add").find('.btn-getitnow-bid').removeClass('hide');
     $("#bid-add").find(".bidBidderRatingScore").html("");
     for (var ratingCpt = 1; ratingCpt < auth.rating; ratingCpt++) {
-      $("#bid-add").find(".bidBidderRatingScore").append('<img width="32" src="https://yoctu.github.io/yoctu-website/img/logo/rating1.png" />');
+      $("#bid-add").find(".bidBidderRatingScore").append('<img width="32" src="https://yoctu.github.io/shaq-view/img/rating1.png" />');
     }
     for (ratingCpt; ratingCpt < 5; ratingCpt++) {
-      $("#bid-add").find(".bidBidderRatingScore").append('<img width="32" src="https://yoctu.github.io/yoctu-website/img/logo/rating0.png" />');
+      $("#bid-add").find(".bidBidderRatingScore").append('<img width="32" src="https://yoctu.github.io/shaq-view/img/rating0.png" />');
     }
   }
-  $('#ChatList').html('<p><button id="Notifications-Chat" class="btn btn-default btn-room"><img class="bidder-image" src="https://yoctu.github.io/yoctu-website/img/logo-svg/notification.svg" /> Notifications <span id="chatBadge-notification" class="badge"></span></button></p>');
+  $('#ChatList').html('<p><button id="Notifications-Chat" class="btn btn-default btn-room"><img class="bidder-image" src="https://yoctu.github.io/shaq-view/img/logo-svg/notification.svg" /> Notifications <span id="chatBadge-notification" class="badge"></span></button></p>');
   $('#ChatList').append('<p><button id="' + window.shaq.source[0] + '" class="btn btn-default btn-room"><img class="bidder-image" src="' + auth.app.logourl + window.shaq.source[0] + '.png" />  ' + window.shaq.sourceName[0] + ' <span id="chatBadge-' + window.shaq.source[0] + '" class="badge">0</span></button></p>');
   if (window.shaq.target && window.shaq.target.includes(auth.auth.usercode)) {
     auth.auth.usercodeName = window.shaq.targetName[window.shaq.target.indexOf(auth.auth.usercode)];
