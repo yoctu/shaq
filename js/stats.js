ffe1c7f35225ee3a023c9e8c32f0577086891c66
git <git@yoctu.com>
2020-06-11 12:03:15 -0400

    test chart

--- js/stats.js
+++ js/stats.js
@@ -1,12 +1,13 @@
 const randomColorGenerator = function() {
   let c = (Math.min(Math.random() * (256), 240) | 0).toString(16)
-  return '#'+((1<<24)*(Math.random()+1)|0).toString(16).substr(1)
-  //return '#' + c + c + c
+  return '#' + ((1 << 24) * (Math.random() + 1) | 0).toString(16).substr(1)
 }
-var date = new Date().toISOString().substring(0, 10)
+var datefrom = new Date().toISOString().substring(0, 10)
+var dateto = new Date().toISOString().substring(0, 10)
 
 $(document).ready(function() {
-  $("#date").val(date)
+  $("#datefrom").val(datefrom)
+  $("#dateto").val(dateto)
   const shaqStatus = document.getElementById('shaqStatus').getContext('2d');
   const bidStatus = document.getElementById('bidStatus').getContext('2d');
   const shaqCompleted = document.getElementById('shaqCompleted').getContext('2d');
@@ -15,11 +16,11 @@ $(document).ready(function() {
   if (bidStatusChart instanceof Chart) bidStatusChart.destroy()
 
   $("#well_stats_created").html(10)
-  $("#well_stats_spent").html(10)
-  $("#well_stats_completed").html(10)
-  $("#well_stats_invited").html(10)
-  $("#well_stats_won").html(10)
-  $("#well_stats_revenue").html(10)
+  $("#well_stats_spent").html(1000 + localSettings.currency.substring(0,1).replace('E','€').replace('D','$'))
+  $("#well_stats_completed").html(8)
+  $("#well_stats_invited").html(5)
+  $("#well_stats_won").html(3)
+  $("#well_stats_revenue").html(1234 + localSettings.currency.substring(0,1).replace('E','€').replace('D','$'))
 
   shaqStatusChart = new Chart(shaqStatus, {
     type: 'pie',
@@ -28,7 +29,7 @@ $(document).ready(function() {
       datasets: [{
         label: 'Shaq Status',
         backgroundColor: ['#4FD844', '#FFC266', '#FF5C16', '#5bc0de'],
-        data: ['25', '70', '1', '4']
+        data: [ 8, 5, 1, 2]
       }]
     },
     options: {
@@ -57,7 +58,7 @@ $(document).ready(function() {
       datasets: [{
         label: 'Shaq Completed',
         backgroundColor: randomColorGenerator,
-        data: [1, 2, 3, 4, 3 , 2]
+        data: [1, 2, 3, 4, 3, 2]
       }]
     },
     options: {
