0df9b858b285bda8a07a26017a8c22df911db13b
lav <lav@yoctu.com>
2020-07-28 10:56:24 +0200

    update version

--- js/auth.js
+++ js/auth.js
@@ -3,7 +3,7 @@ const qs = new URLSearchParams(window.location.search)
 var auth = {};
 auth.auth = {};
 
-auth.webversion = "1.4.7-1";
+auth.webversion = "1.4.7-2";
 
 function getCookie(cname) {
   var name = cname + "=";
